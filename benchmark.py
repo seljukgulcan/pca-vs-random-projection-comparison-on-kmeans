e62958d42d4743017c73db034af4a149302039b8
Shathra <selcuk.gulcan@bilkent.edu.tr>
2022-05-25 15:44:12 +0300

    rp runs 5 times

--- benchmark.py
+++ benchmark.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
         n_img = int(sys.argv[5])
     else:
         n_img = len(filename_lst)
+
     result_filepath = Path('result') / f'{dataset}_{n_img}_{width}_{d_pca}_{k}.csv'
 
     # Read the images
@@ -73,29 +74,32 @@ if __name__ == '__main__':
 
     # Clustering using Random Projection
 
-    n_component_lst = [100, 200, 400, 800, 1000, 2000, 4000]
-    elapsed_lst = []
-    error_lst = []
+    # Repeat the experiment 5 times
 
-    for n_component in n_component_lst:
+    for _ in range(5):
+        n_component_lst = [100, 200, 400, 800, 1000, 2000, 4000]
+        elapsed_lst = []
+        error_lst = []
+
+        for n_component in n_component_lst:
+            
+            t_start = time.perf_counter()
+            
+            # Reduce dimensions
+            mdl = GaussianRandomProjection(n_components=n_component)
+            X_tr = mdl.fit_transform(X)
+            
+            # Cluster
+            mdl = KMeans(n_clusters=k)
+            mdl.fit(X_tr)
+            
+            t_end = time.perf_counter()
+            elapsed = t_end - t_start
+            error = calc_error(X, mdl.labels_, k)
+            
+            elapsed_lst.append(elapsed)
+            error_lst.append(error)
         
-        t_start = time.perf_counter()
-        
-        # Reduce dimensions
-        mdl = GaussianRandomProjection(n_components=n_component)
-        X_tr = mdl.fit_transform(X)
-        
-        # Cluster
-        mdl = KMeans(n_clusters=k)
-        mdl.fit(X_tr)
-        
-        t_end = time.perf_counter()
-        elapsed = t_end - t_start
-        error = calc_error(X, mdl.labels_, k)
-        
-        elapsed_lst.append(elapsed)
-        error_lst.append(error)
-    
-    with open(result_filepath, 'a') as file:
-        for n, elapsed, error in zip(n_component_lst, elapsed_lst, error_lst):
-            file.write(f'{n},{elapsed},{error}\n')
+        with open(result_filepath, 'a') as file:
+            for n, elapsed, error in zip(n_component_lst, elapsed_lst, error_lst):
+                file.write(f'{n},{elapsed},{error}\n')
